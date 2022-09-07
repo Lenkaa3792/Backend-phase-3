78a4d0746f6849a3fdc23ae51059f451a80eab76
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-08 00:03:24 +0300

    inserting data that is available in Faker

--- db/migrate/20220907122801_buses.rb
+++
@@ -1,9 +0,0 @@
-class Buses < ActiveRecord::Migration[7.0]
-  def change
-      create_table :buses do |t|
-      t.string :BusNo
-      t.integer :capacity
-      t.string :Route
-    end
-  end
-end
--- db/migrate/20220907123115_routes.rb
+++
@@ -1,8 +0,0 @@
-class Routes < ActiveRecord::Migration[7.0]
-  def change
-    create_table :routes do |t|
-      t.string :RouteName
-      t.integer :BusNo
-  end
-end
-end
