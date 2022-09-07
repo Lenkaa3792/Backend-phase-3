e9c39fa4aeb9941ab596069c55e43e56f2da7886
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-07 21:44:05 +0300

    created migrations

---
+++ db/migrate/20220907123102_passengers.rb
@@ -0,0 +1,11 @@
+class Passengers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :passengers do |t|
+      t.string :Name
+      t.integer :Phone
+      t.string :Route
+      t.string :BusNo
+
+    end
+  end
+end
