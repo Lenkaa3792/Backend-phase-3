961c842f56df69e66b39b4f2ed9f468c3ae892e7
Lenkaa3792 <allanlenkaa@gmail.com>
2022-09-08 11:43:17 +0300

    route seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,30 @@ Buses.create(
 #      t.string :insurance
 #       t.integer :phone
 #        t.string :vehicle_registration
+15.times do
+    Passengers.create(
+        Name: Faker::Name.name,
+        phone: Faker::Phone.number,
+        location: "Mombasa"
 
+
+    )
+end
 
 # create_table "passengers", force: :cascade do |t|
 #     t.string "name"
 #     t.integer "phone"
 #     t.string "location"
+#   end
+
+15.times do
+    Routes.create(
+        location: "Mombasa",
+        Name: Faker::Name.name
+
+    )
+end
+#      create_table "routes", force: :cascade do |t|
+#     t.string "location"
+#     t.string "name"
 #   end
